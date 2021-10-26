e5150f2ebcafac15dacbd3b3ff8461a28cea4899
abyanrf <rabyan78@gmail.com>
2021-10-26 22:09:13 +0700

    A

---
+++ Tugas2 – String Conditional/index.js
@@ -0,0 +1,172 @@
+//  tugas string
+
+
+console.log('no1');
+var word = "javaScript";
+var second = "is";
+var third = "awesome";
+var fourth = "and";
+var fifth = "I";
+var sixt = "love";
+var seventh = "it!";
+
+console.log(`${word} ${second} ${third} ${fourth} ${fifth} ${sixt} ${seventh}`);
+
+console.log('no2');
+var sentence = "I am going to be React Native Developer";
+
+var exampleFirstWord = sentence[0];
+var exampleSecondWord = sentence[2] + sentence[3];
+var thirdWord =
+  sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9];
+var fourthWord = sentence[11] + sentence[12];
+var fifthWord = sentence[14] + sentence[15];
+var sixthWord =
+  sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21];
+var seventhWord =
+  sentence[23] +
+  sentence[24] +
+  sentence[25] +
+  sentence[26] +
+  sentence[27] +
+  sentence[28];
+var eighthWord =
+  sentence[30] +
+  sentence[31] +
+  sentence[32] +
+  sentence[33] +
+  sentence[34] +
+  sentence[35] +
+  sentence[36] +
+  sentence[37] +
+  sentence[38];
+
+console.log("First Word: " + exampleFirstWord);
+console.log("Second Word: " + exampleSecondWord);
+console.log("Third Word: " + thirdWord);
+console.log("Fourth Word: " + fourthWord);
+console.log("Fifth Word: " + fifthWord);
+console.log("Sixth Word: " + sixthWord);
+console.log("Seventh Word: " + seventhWord);
+console.log("Eighth Word: " + eighthWord);
+
+console.log("no3");
+var sentence2 = "wow JavaScript is so cool";
+
+var exampleFirstWord2 = sentence2.substring(0, 3);
+var secondWord2 = sentence2.substring(4, 15);
+var thirdWord2 = sentence2.substring(15, 17);
+var fourthWord2 = sentence2.substring(18, 20);
+var fifthWord2 = sentence2.substring(21);
+
+console.log("First Word: " + exampleFirstWord2);
+console.log("Second Word: " + secondWord2);
+console.log("Third Word: " + thirdWord2);
+console.log("Fourth Word: " + fourthWord2);
+console.log("Fifth Word: " + fifthWord2);
+
+console.log("no4");
+
+var sentence3 = "wow JavaScript is so cool";
+
+var exampleFirstWord3 = sentence3.substring(0, 3);
+var secondWord3 = sentence3.substring(4, 14);
+var thirdWord3 = sentence3.substring(15, 17);
+var fourthWord3 = sentence3.substring(18, 20);
+var fifthWord3 = sentence3.substring(21);
+
+var firstWordLength = exampleFirstWord3.length;
+var secondWordLength = secondWord3.length;
+var thirdWordLength = thirdWord3.length;
+var fourthWordLength = fourthWord3.length;
+var fifithWordLength = fifthWord3.length;
+
+console.log(
+  "First Word: " + exampleFirstWord3 + ", with length: " + firstWordLength
+);
+console.log(
+  "Second Word: " + secondWord3 + ", with length: " + secondWordLength
+);
+console.log("Third Word: " + thirdWord3 + ", with length: " + thirdWordLength);
+console.log(
+  "Fourth Word: " + fourthWord3 + ", with length: " + fourthWordLength
+);
+console.log("Fifth Word: " + fifthWord3 + ", with length: " + fifithWordLength);
+
+console.log("no 5");
+
+console.log("if-else");
+var nama = "abyan";
+var peran = "Guard";
+
+if (!nama) {
+  console.log("nama harus di isi");
+}
+if (!peran) {
+  console.log(`hai ${nama},pilih peran mu untuk memulai game `);
+}
+if (peran == "Penyihir") {
+  console.log(`Selamat datang di Dunia Werewolf,${nama}`);
+  console.log(
+    `Halo Penyihir ${nama}, kamu dapat melihat siapa yang menjadi werewolf!`
+  );
+} else if (peran == "Guard") {
+  console.log(`Selamat datang di Dunia Werewolf,${nama}`);
+  console.log(
+    `Halo Guard ${nama}, kamu akan membantu melindungi temanmu dari serangan werewolf.`
+  );
+} else if (peran == "WereWolf") {
+  console.log(`Selamat datang di Dunia Werewolf,${nama}`);
+  console.log(`Halo Werewolf ${nama}, Kamu akan memakan mangsa setiap malam!`);
+}
+
+console.log("switch case");
+
+var hari = 21;
+var bulan = 6;
+var tahun = 1945;
+var bulanStr = console.log("");
+
+switch (bulan) {
+  case 1:
+    bulanStr = "januari";
+    break;
+  case 2:
+    bulanStr = "febuari";
+    break;
+  case 3:
+    bulanStr = "maret";
+    break;
+  case 4:
+    bulanStr = "april";
+    break;
+  case 5:
+    bulanStr = "mei";
+    break;
+  case 6:
+    bulanStr = "juni";
+    break;
+  case 7:
+    bulanStr = "juli";
+    break;
+  case 8:
+    bulanStr = "agustus";
+    break;
+  case 9:
+    bulanStr = "september";
+    break;
+  case 10:
+    bulanStr = "oktober";
+    break;
+  case 11:
+    bulanStr = "november";
+    break;
+  case 12:
+    bulanStr = "desember";
+    break;
+
+  default:
+    break;
+}
+
+console.log(`${hari} ${bulanStr} ${tahun}`);
