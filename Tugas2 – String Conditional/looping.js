2fcb89905293feed5e1bb9b96e6f10a3a6592e55
abyanrf <rabyan78@gmail.com>
2021-10-28 13:35:21 +0700

    C

---
+++ Tugas2 – String Conditional/looping.js
@@ -0,0 +1,14 @@
+// var apel =1
+
+// while (apel < 12) {
+//     console.log('apel ke' + apel);
+//     apel++    
+// }console.log('looping selesai');
+
+var deret = 3;
+var jumlah = 0;
+while (deret > 0) {
+    jumlah += deret;
+    deret--;
+    console.log('jumlah sekarang' + jumlah);
+}
