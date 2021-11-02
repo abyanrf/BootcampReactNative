42bb80ecbf353bb98c28e077e2e7c6cf142571d2
abyanrf <rabyan78@gmail.com>
2021-11-02 08:54:56 +0700

    tugas

--- Tugas6–Object/object.js
+++ Tugas6–Object/object.js
@@ -1,40 +1,79 @@
-// var people = {
-//   "1. Abduh Muhammad": ["Abduh", "Muhammad", "male", 1992],
-//   "2. Ahmad Taufik": ["Ahmad", "Taufik", "male", 1985],
-// };
-// var people2 = [
-//   ["Tony", "Stark", "male", 1980],
-//   ["Pepper", "Pots", "female", 2023],
-// ];
+console.log('no 1');
+console.log("\n");
 
-// function arrayToObject(arr) {
-//   var date = new Date();
-//   var data1 = date.getFullYear() - arr["1. Abduh Muhammad"][3];
-//   var data2 = date.getFullYear() - arr["2. Ahmad Taufik"][3];
+var input =  [["Abduh", "Muhamad", "male", 1992], ["Ahmad", "Taufik", "male", 1985]]
 
-//   var people1 = {
-//     firstName: arr["1. Abduh Muhammad"][0],
-//     lastName: arr["1. Abduh Muhammad"][1],
-//     gender: arr["1. Abduh Muhammad"][2],
-//     age: data1,
-//   };
-
-//   var people2 = {
-//     firstName: arr["2. Ahmad Taufik"][0],
-//     lastName: arr["2. Ahmad Taufik"][1],
-//     gender: arr["2. Ahmad Taufik"][2],
-//     age: data2,
-//   };
-
-//   console.log("1. Abduh Muhammad : ", people1);
-//   console.log("\n");
-//   console.log("2. Ahmad Taufik : ", people2);
-// }
-// arrayToObject(people);
-
-
-
-let arrayToObject = ()=>{
+function arrayToObject(input) {
+    var now = new Date();
+    var thisYear =now.getFullYear()
+    var temp = thisYear -  input[0][3]
+    var umur =temp ? temp : 'Invalid Birth Year'
+        
     
-} 
+    var temp1 = thisYear - input[1][3]
+    var umur1 =temp1 ? temp1 : 'Invalid Birth Year'
+
+  var people = {};
+  (people.firstname = input[0][0]),
+    (people.lastname = input[0][1]),
+    (people.gender = input[0][2]),
+    people.age =umur
+
+
+  var people2 = {};
+  (people2.firstname = input[1][0]),
+    (people2.lastname = input[1][1]),
+    (people2.gender = input[1][2]),
+    people2.age=umur1
+
+  console.log("1.", people.firstname, " ", people.lastname, " : ", people);
+  console.log("\n");
+  console.log("2.", people2.firstname, " ", people2.lastname, " : ", people2);
+  
+}
+arrayToObject(input);
+
+console.log("\n");
+console.log('no2 ');
+console.log("\n");
+function shoppingTime(memberId, money){
+    changeMoney: 0 
+    var stok=[
+        {
+            nama:'sepatu Stacattu ',
+            harga:1500000
+        },
+        {
+            nama:'baju zoro',
+            harga:500000
+        },
+        {
+            nama:'baju H&N',
+            harga:250000
+        },
+        {
+            nama:'uniklohh',
+            harga:175000
+        },
+        {
+            nama:'casing hp',
+            harga:50000
+        }
+    ]
+
+    if(memberId === undefined){
+        console.log('“Mohon maaf, toko X hanya berlaku untuk member saja”');
+    }if(money <= 50000){
+        console.log('“Mohon maaf, uang tidak cukup”');
+    }for(i=0;i<stok.length;i++){
+        changeMoney= money - stok[0].harga - stok[1].harga - stok[2].harga - stok[3].harga - stok[4].harga 
+    }
+    console.log('memberId :'+ memberId);
+    console.log('money : ' + money);
+    console.log('changeMoney : ' + changeMoney);
+}
+shoppingTime('1820RzKrnWn08', 2475000)
+console.log("\n");
+
+console.log('maaf ka blm sempet mengejakan  no 4');
 
