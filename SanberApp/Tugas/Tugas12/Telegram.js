a6ac91a2172e61dff0c36dee38554a75f28c6475
abyanrf <rabyan78@gmail.com>
2021-11-10 08:46:38 +0700

    apps

---
+++ SanberApp/Tugas/Tugas12/Telegram.js
@@ -0,0 +1,191 @@
+import React from "react";
+import {
+  View,
+  Image,
+  Text,
+  StatusBar,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+import Data from "./dummy";
+
+const colors = {
+  primary: "#517DA2",
+  secondary: "#4EA4EA",
+  caption: "#A8AA",
+};
+
+const Telegram = () => {
+  return (
+    <SafeAreaView style={style.container}>
+      <StatusBar barStyle='light-content' backgroundColor="#41688d" />
+      <View style={style.header}>
+        <TouchableOpacity style={{ marginHorizontal: 20 }}>
+          <Image
+            source={require("../Tugas12/asset/drawwer.png")}
+            style={style.listHeader}
+          />
+        </TouchableOpacity>
+        <Text style={style.textHeader}>Telegram</Text>
+        <TouchableOpacity style={{ marginRight: 20 }}>
+          <Image source={require("../Tugas12/asset/search1-.png")} />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={Data}
+        renderItem={({ item }) => (
+          <>
+            <TouchableOpacity style={style.kotakPesan}>
+              <Image source={item.image} style={style.potoProfile} />
+              <View style={style.pesan}>
+                <Text style={style.pengirimPesan}>
+                  {item.name}
+                </Text>
+                <Text style={style.isiPesan}>{item.message}</Text>
+              </View>
+              <View style={style.statusPesan}>
+                <View style={style.status2}>
+                  <Image
+                    style={style.centang}
+                    source={require("./asset/check.png")}
+                  />
+                  <Text style={style.statusWaktu}>{item.time}</Text>
+                </View>
+                <Text
+                  style={style.jumlahPesan}
+                >
+                  {item.totalMessage}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </>
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={style.separator}
+          />
+        )}
+      />
+
+      <TouchableOpacity
+        style={style.pencil}
+      >
+        <Image
+          source={require("./asset/pencil.png")}
+          style={style.iconPencil}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  iconPencil:{ 
+    width: 23, 
+    height: 25, 
+    resizeMode: "contain" 
+  },
+
+  pencil:{
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+
+  separator:{
+    borderBottomWidth: 1,
+    opacity: 0.1,
+    marginRight: 20,
+    marginLeft: 90,
+  },
+
+  jumlahPesan:{
+    marginTop: 5,
+    color: "white",
+    fontSize: 12,
+    width: 18,
+    height: 18,
+    backgroundColor: "#4ECC5E",
+    borderRadius: 10,
+    alignSelf: "flex-end",
+    textAlign: "center",
+  },
+
+  centang:{ 
+    width: 10, 
+    height: 10, 
+    marginRight: 6
+   },
+
+  statusWaktu:{
+    color:colors.caption
+  },
+
+  status2:{ 
+    flexDirection: "row", 
+    alignItems: "center" 
+  },
+
+  statusPesan:{ 
+    justifyContent: "space-around" 
+  },
+
+  isiPesan:{
+    color:colors.caption
+  },
+
+  pengirimPesan:{
+     fontSize: 17, 
+     fontWeight: "bold" 
+    }
+  ,
+  pesan: { 
+    flex: 1, 
+    marginHorizontal: 15 },
+  potoProfile: {
+    width: 60,
+    height: 55,
+  },
+  kotakPesan: {
+    height: 86,
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 15,
+    marginBottom: 2,
+    marginTop: 2,
+  },
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    backgroundColor: colors.primary,
+    height: 62,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  listHeader: {
+    width: 27,
+    height: 21,
+  },
+  textHeader: {
+    fontSize: 27,
+    color: "white",
+    flex: 1,
+    marginLeft: 15,
+    fontWeight: "600",
+  },
+});
+
+export default Telegram;
--- Tugas8– Asynchronous/index.js
+++ Tugas8– Asynchronous/index.js
@@ -1,48 +1,8 @@
-const readBooks = require('./callback.js')
-
-const books = [
-    {name: 'LOTR', timeSpent: 3000},
-    {name: 'Fidas', timeSpent: 2000},
-    {name: 'Kalkulus', timeSpent: 4000}
-]
-readBooks(books.timeSpent,books.name,()=>{
-    
+const myPromise = new Promise ((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve('timer completed')
+  },1000)
 })
-
-function Clock({ template }) {
-  
-    var timer;
-  
-    function render() {
-      var date = new Date();
-  
-      var hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      var mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      var secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      var output = template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    this.stop = function() {
-      clearInterval(timer);
-    };
-  
-    this.start = function() {
-      render();
-      timer = setInterval(render, 1000);
-    };
-  
-  }
-  
-  var clock = new Clock({template: 'h:m:s'});
-  clock.start(); 
+.then((text)=>{throw new Error('failed')})
+.catch(err => console.log(err))
+.then(()=>console.log('dos ?'))
